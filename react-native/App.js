ee3ec4a907f538a2d494b82a9ee85c7c3fe4287b
Matthew Hsu <gears88@gmail.com>
2017-12-01 01:04:21 -0500

    Refactor authentication flow for future api call support

--- react-native/App.js
+++ react-native/App.js
@@ -1,20 +1,81 @@
 import React from 'react';
 import {StyleSheet, Text, TextInput, View, KeyboardAvoidingView, Image, ActivityIndicator} from 'react-native';
+import {Root} from 'native-base';
 
-import CodeScreen from './js/EnterCode';
-import PhoneScreen from './js/EnterPhone';
+import EnterAuth from './js/components/EnterAuth';
 import JobScreen from './js/EnterJob';
 import AcceptScreen from './js/Acceptance'
 import { StackNavigator } from 'react-navigation';
 
-const SimpleApp = StackNavigator({
-    ENTER_PHONE: {screen: PhoneScreen},
-    ENTER_CODE: {screen: CodeScreen},
+const AppNavigator = StackNavigator({
     ENTER_JOB: {screen: JobScreen},
     ACCEPT: {screen: AcceptScreen},
 });
-export default SimpleApp;
 
+const AUTH_STATES = Object.freeze({phone: 1, code: 2, auth: 3});
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fontsAreLoaded: false,
+            authState: AUTH_STATES.phone
+        }
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.setState({fontsAreLoaded: true});
+    }
+
+    authenticate = async(input) => {
+        console.log("auth", input);
+        console.log(this.state.authState);
+        switch (this.state.authState) {
+            case AUTH_STATES.phone:
+                //todo auth with server
+                this.setState({authState: AUTH_STATES.code});
+                console.log("state -> code")
+                break;
+            case AUTH_STATES.code:
+                //todo auth with server
+                this.setState({authState: AUTH_STATES.auth});
+                break;
+        }
+        return true;
+    };
+
+    render() {
+        if (!this.state.fontsAreLoaded) {
+            return <View><Text>Loading</Text></View>;
+        }
+        if (this.state.authState !== AUTH_STATES.auth) {
+            return (
+                //<Provider store={store}>
+                    <Root>
+                        {this.state.authState === AUTH_STATES.phone
+                        ? <EnterAuth onSubmit={this.authenticate} textBoxLabel="Enter Phone" buttonLabel="SEND CODE"/>
+                        : <EnterAuth onSubmit={this.authenticate} textBoxLabel="Enter Code" buttonLabel="SEND"/>}
+                    </Root>
+                //</Provider>
+            );
+        }
+
+        return (
+           // <Provider store={store}>
+                <Root>
+                    <AppNavigator/>
+                </Root>
+            //</Provider>
+        )
+
+    }
+}
 
 // Possible Screen States (JS doesn't have Enum's)
 // const ENTER_PHONE = 'ep';
