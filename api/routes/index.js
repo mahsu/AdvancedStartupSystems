6e827d989239763ebb9ecbcf2551a209d31a1ccc
Matthew Hsu <gears88@gmail.com>
2017-11-30 22:54:28 -0500

    Use twilio to verify authenticity of phone number

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,31 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('../config');
+
+const twilio = require('twilio')(config.twilio.prod_account, config.twilio.prod_token);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.post("/auth/number", function(req, res, next) {
+router.post("/auth/phone", function (req, res, next) {
+    twilio.lookups.v1
+        .phoneNumbers(req.body.phone)
+        .fetch()
+        .then((number) => {
+            console.log(number.phoneNumber);
+            return res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.sendStatus(500);
+        });
+});
+
+router.post("/auth/code", function (req, res, next) {
 
 });
 
-router.post("/auth/code", function(req, res, next) {
+
+router.get("/jobs", function (req, res, next) {
 
 });
 
-
-router.get("/jobs", function(req, res, next) {
-
-});
-
-router.put('/job/new', function(req, res, next) {
-  //create job in db
+router.put('/job/new', function (req, res, next) {
+    //create job in db
     //match with mover
     //return details of job + mover
 });
 
-router.put('/job/{jobid}', function(req, res, next) {
+router.put('/job/{jobid}', function (req, res, next) {
 
 })
 module.exports = router;
